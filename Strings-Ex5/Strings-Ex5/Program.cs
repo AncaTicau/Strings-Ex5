96a43d246d282ca34415f3c71064c994b44ec002
MariusA <anca.ticau@gmail.com>
2019-06-06 22:57:39 +0300

    Exercise 5

---
+++ Strings-Ex5/Strings-Ex5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace Strings_Ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string info = StringReverse("This is a string");
+
+            Console.WriteLine(info);
+        }
+
+        //Write a method that reverses a string.
+
+        public static string StringReverse(string input)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                sb.Append(input[i]);
+            }
+
+            return sb.ToString();
+        }
+
+    }
+
+}
